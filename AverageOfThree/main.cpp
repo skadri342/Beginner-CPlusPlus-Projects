3dc9a42bc3560c84cbe60ba4b9d23f2ad49752eb
Shams Kadri <quadrishams342@gmail.com>
2023-11-20 15:41:12 -0500

    made final changes

--- AverageOfThree/main.cpp
+++ AverageOfThree/main.cpp
@@ -2,10 +2,20 @@
 using namespace std;
 
 int main() {
-    int myInt;
-    myInt = 15;
 
-    cout << myInt << endl;
+    double num1, num2, num3;
+    double average;
+
+    cout << "Please enter a real number" << endl;
+    cin >> num1;
+    cout << "Now, enter another real number" << endl;
+    cin >> num2;
+    cout << "Now, enter the final real number" << endl;
+    cin >> num3;
+
+    average = (num1 + num2 + num3)/3;
+
+    cout << "Average is " << average << endl;
 
     return 0;
 }
