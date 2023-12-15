35650172e9933807732fd81e79fe9ab9bd11d607
Shams Kadri <quadrishams342@gmail.com>
2023-12-14 22:34:57 -0500

    added project requirements

--- Jam-of-the-Month-Club/main.cpp
+++ Jam-of-the-Month-Club/main.cpp
@@ -4,14 +4,32 @@
 using namespace std;
 
 int main() {
-    //generate 10 die rolls and print out each as you go along
-    srand(time(nullptr));
-    int die_roll;
-
-    for (int i = 0; i < 10; i++) {
-        die_roll = rand() % 6 + 1;
-        cout << die_roll << endl;
-    }
+    //Users join a club offering jams, jellies, and preserves
+    //They get a certain number of these per month, and extras on top of that cost more
+    //
+    //You should first prompt the user for which package they have, from the following:
+    //Package A:
+    //  $8/month
+    //  Includes 2 jams per month
+    //  Each additional jam on top of the 2 is $5 each
+    //
+    //Package B:
+    //  $12/month
+    //  Includes 4 jams per month
+    //  Each additional jam on top of the 4 is $4 each
+    //
+    //Package C:
+    //  $15/month
+    //  Includes 6 jams per month
+    //  Each additional jam on top of the 6 is $3 each
+    //
+    //You should secondly prompt the user for how many jams they have purchased this month, in total
+    //The program will then take the number of jams purchased and then tell the user their total cost
+    //  This will depend on what package they own
+    //  If they stayed at or under the number of jams included in their monthly package, they only owe
+    //  the monthly fee
+    //  If they went over, then you charge them the monthly fee, plus the charge for each additional jam
+    //  at their package's rate
 
     return 0;
 }
