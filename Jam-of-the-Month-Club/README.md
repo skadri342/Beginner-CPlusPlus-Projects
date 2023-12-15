2dc1294d779f0defc4043f422f04ea636f2915fd
Shams Kadri <quadrishams342@gmail.com>
2023-12-14 21:59:53 -0500

    learned about continue and break statements

--- Jam-of-the-Month-Club/README.md
+++ Jam-of-the-Month-Club/README.md
@@ -1,4 +1,5 @@
 Learned about control statements. Sequential statements (sequence), 
 selection statements (decision), and repetition statements (loops).
 Learned about if statements and switch case syntax for C++. Also 
-learned about while, do-while, and for loop syntax.
+learned about while, do-while, and for loop syntax. Saw the different
+effects of the continue and break statement.
--- Jam-of-the-Month-Club/main.cpp
+++ Jam-of-the-Month-Club/main.cpp
@@ -2,23 +2,16 @@
 using namespace std;
 
 int main() {
-    // ask user for input
-    // add user input to a sum/running total
-    // when user enter's a negative number, the loop terminates and prints the total sum
-    int input;
-    int sum = 0;
+    int count = 0;
 
-    cout << "Please enter a non-negative integer, enter a negative integer to quit" << endl;
-    cin >> input;
-
-    while (input >= 0) {
-        cout << input << endl;
-        sum += input;
-        cout << "Please enter a non-negative integer, enter a negative integer to quit" << endl;
-        cin >> input;
+    while (count < 10) {
+        if (count == 5) {
+            count++;
+            continue;
+        }
+        cout << count << endl;
+        count++;
     }
-
-    cout << "Sum of the values: " << sum << endl;
 
     return 0;
 }
