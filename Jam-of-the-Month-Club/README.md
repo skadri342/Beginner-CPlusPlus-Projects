71c19dbfec81eed5b92ee2b29d9198f64c2e991a
Shams Kadri <quadrishams342@gmail.com>
2023-12-14 21:43:35 -0500

    learned about for loops

--- Jam-of-the-Month-Club/README.md
+++ Jam-of-the-Month-Club/README.md
@@ -1,3 +1,4 @@
 Learned about control statements. Sequential statements (sequence), 
 selection statements (decision), and repetition statements (loops).
-Learned about if statements and switch case syntax for C++.
+Learned about if statements and switch case syntax for C++. Also 
+learned about while, do-while, and for loop syntax.
--- Jam-of-the-Month-Club/main.cpp
+++ Jam-of-the-Month-Club/main.cpp
@@ -2,25 +2,26 @@
 using namespace std;
 
 int main() {
-    //Prompt user for age AND gender
-    //age is int, gender is character
-    //get of f or F and are 60 or over
-    //You qualify for the discount!
-    //You do not qualify for the discount.
-    int age;
-    char gender;
+//    int count = 0;
+//
+//    while (count < 10) {                //loop continuation condition
+//        cout << count << endl;
+//        count++;
+//    }
+//
+//    cout << endl << endl;
+//
+//    //do while
+//    int count2 = 10;
+//    do {
+//        cout << count2 << endl;
+//        count2++;
+//    } while (count2 < 10);
 
-    cout << "Please enter your age:" << endl;
-    cin >> age;
-
-    cout << "Please enter your gender (M or F):" << endl;
-    cin >> gender;
-
-    if (age >= 60 && (gender == 'f' || gender == 'F')) {
-        cout << "You qualify for the discount!" << endl;
-    } else {
-        cout << "You do not qualify for the discount." << endl;
-    }
+//    //for loop
+//    for (int i = 0; i < 10; i++) {
+//        cout << i << endl;
+//    }
 
     return 0;
 }
